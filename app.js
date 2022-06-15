e62f7338994da5672010cb7d713fbbf09dfd9832
mohammedDBS <mohammed.dbs.660@gmail.com>
2022-06-15 15:28:40 +0200

    start my assignment

---
+++ app.js
@@ -0,0 +1,20 @@
+(function(){
+
+    angular.module('LunchCheck',[])
+
+    .controller('adding',LunchCheckController)
+
+    LunchCheckController.$inject =['$scope'];
+
+    function LunchCheckController($scope){
+        $scope.c = function (){}
+    }
+
+
+
+
+
+
+
+
+})()
