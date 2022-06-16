7dfb8dc95bb9345c3e42087802fabd8f26383779
mohammed dbs <mohammed.dbs.660@gmail.com>
2022-06-16 13:01:33 +0200

    find the solution but hv 1 err

--- app.js
+++ app.js
@@ -7,8 +7,18 @@
     LunchCheckController.$inject =['$scope'];
 
     function LunchCheckController($scope){
-        $scope.c = function (){}
-    }
+        $scope.foods = "";
+        $scope.givemessage = function(foods){
+            foods = $scope.foods
+            
+            var xfood=foods.split(",")
+            var foodsl = xfood.length
+            if (foodsl<=3 && foodsl!=0){return "Enjoy!"}
+            else if(foodsl>3){return "Too much !"}
+            else {return 'empty, put some data'}
+        }
+        }
+    
 
 
 
